5b1529f7d17a11719ab5a36ab4fe2b6ad2b9caa7
massimopallera <pallera.massimo@gmail.com>
2024-11-11 16:07:40 +0100

    added backgrounds

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,53 +11,65 @@ export default function Main(){
   //BONUS:
   return (
   <>
-      <div>
-        <h2>Current Tasks ({inProgress.length})</h2>
+    <main>
 
-        <ul>
+      <div className="container">
 
-          {inProgress.map((task, index) => {
-            return (
+        {/* In progress tasks */}
+        <div>
+          <h2>Current Tasks ({inProgress.length})</h2>
+
+          <ul>
+
+            {inProgress.map((task, index) => {
+              return (
+                <li key={index}>
+                <div>
+                  <div>
+                    <strong>{task.title}</strong> 
+                    <div className='state'>{task.state}</div>
+                  </div>
+                  <p>Priority: {task.priority}</p>
+                  <p>Est. Time: {task.estimatedTime}</p>
+                </div>
+              </li>
+            )}
+            )}
+
+          </ul>
+
+        </div>
+
+        <br />
+
+        {/* Completed Tasks */}
+        <div>
+          <h2>Completed Tasks ({completed.length}) </h2>
+
+          <ul>
+
+            {completed.map((task, index) => {
+              return (
               <li key={index}>
-              <div>
-                <span>
-                  <strong>{task.title}</strong> 
-                  <div className='state'>{task.state}</div>
-                </span>
-                <p>Priority: {task.priority}</p>
-                <p>Est. Time: {task.estimatedTime}</p>
-              </div>
-            </li>
-          )}
-          )}
+                <div>
+                  <div>
+                    <strong>{task.title}</strong> 
+                    <div className='state'>{task.state}</div>
+                  </div>
+                  <p>Priority: {task.priority}</p>
+                  <p>Est. Time: {task.estimatedTime}</p>
+                </div>
+              </li>
+            )}
+            )}
 
-        </ul>
-
+          </ul>
+    
+        </div>
+      
       </div>
 
-      <div>
-        <h2>Completed Tasks ({completed.length}) </h2>
-
-        <ul>
-
-          {completed.map((task, index) => {
-            return (
-            <li key={index}>
-              <div>
-                <span>
-                  <strong>{task.title}</strong> 
-                  <div className='state'>{task.state}</div>
-                </span>
-                <p>Priority: {task.priority}</p>
-                <p>Est. Time: {task.estimatedTime}</p>
-              </div>
-            </li>
-          )}
-          )}
-
-        </ul>
-  
-      </div>
+    </main>
   </>
 
   )
